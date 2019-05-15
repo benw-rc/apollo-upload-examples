2520766859b026f199eb5f74a70671295b47a611
Ben Warzeski <ben.warzeski@readcoor.com>
2019-05-15 16:31:43 -0400

    fix mutations

--- frontend/src/modules/api/store/thunk_actions.js
+++ frontend/src/modules/api/store/thunk_actions.js
@@ -1,13 +1,27 @@
 import gql from 'graphql-tag';
+
 import { mutate } from 'rc-modules/graphql/store/thunk_actions';
+import { S3Location } from '../variables';
+
 export const upload_file = (file) => {
+  const input = {
+    config: {
+      PROJECT_NAME: 'testing project',
+      PANEL_NAME: 'my panel',
+      PANEL_TYPE: 'panel type',
+      EMAILS: 'scientist@rc.com',
+    },
+    file,
+    s3Location: S3Location
+  };
+    
   return mutate(
-    gql`mutation($file: Upload!) {
-      singleUpload(file: $file) {
+    gql`mutation($input: DocumentInput!) {
+      singleUpload(input: $input) {
         id
       }
     }`,
-    { file },
-    console.log
+    { input },
+    () => ({})
   )
 }
---
+++ frontend/src/modules/api/variables/index.js
@@ -0,0 +1,4 @@
+export const S3Location = {
+  bucket: process.env.REACT_APP_APOLLO_BUCKET,
+  key: process.env.REACT_APP_APOLLO_KEY,
+};
